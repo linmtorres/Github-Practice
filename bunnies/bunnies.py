f1d61aaf2c55e3b521522a3448ed8cb49908740c
linmtorres <132651087+linmtorres@users.noreply.github.com>
2023-05-06 14:09:05 +0100

    Update bunnies.py

--- bunnies/bunnies.py
+++ bunnies/bunnies.py
@@ -1,8 +1,18 @@
 def print_bunny_pyramid():
-    word = "BUNNY"
-    for i in range(1, 6):
-        spaces = "   " * (5 - i)
+    word = "KITTY"
+    word2 = "BUNNY"
+    word3 = "SNAIL"
+    word4 = "BIRD"
+    for i in range(1, 11):
+        spaces = " ~ " * (11 - i)
         layer_word = " ".join([word] * i)
         print(spaces + layer_word + spaces)
+    sneak_kiss(3)
 
+def sneak_kiss(count):
+    for i in range(1,count):
+        print("sneak kiss!")
+    
+sneak_kiss(3)
 print_bunny_pyramid()
+
